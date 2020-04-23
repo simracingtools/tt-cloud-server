def1b77b04d75a48d1c2c410cd7c3325e3066a31
Robert Bausdorf <r.bausdorf@clevershuttle.de>
2020-04-24 01:28:59 +0200

    Appengine fixes

--- src/main/java/de/bausdorf/simcacing/tt/Application.java
+++ src/main/java/de/bausdorf/simcacing/tt/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-//@EnableAdminServer
 @EnableConfigurationProperties
 //@EnableDatastoreRepositories
 public class Application {
--- src/main/java/de/bausdorf/simcacing/tt/web/security/WebSecurityConfig.java
+++ src/main/java/de/bausdorf/simcacing/tt/web/security/WebSecurityConfig.java
@@ -1,24 +1,14 @@
 package de.bausdorf.simcacing.tt.web.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @Configuration
 @EnableWebSecurity
-@EnableOAuth2Sso
-@ConditionalOnBean(ClientRegistrationRepository.class)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     GoogleUserService userService;
@@ -27,30 +17,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         this.userService = userService;
     }
 
-    @Bean
-    @ConditionalOnMissingBean
-    public OAuth2AuthorizedClientService authorizedClientService(
-            ClientRegistrationRepository clientRegistrationRepository) {
-        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
-    }
-
-    @Bean
-    public PrincipalExtractor principalExtractor() {
-        return new GooglePrincipalExtractor(userService);
-    }
+//    @Bean
+//    @ConditionalOnMissingBean
+//    public OAuth2AuthorizedClientService authorizedClientService(
+//            ClientRegistrationRepository clientRegistrationRepository) {
+//        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository);
+//    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/clientmessage", "/_ah/start", "/_ah/health");
+        web.ignoring().antMatchers("/clientmessage", "/_ah/**");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-    			.antMatchers("/clientmessage", "/_ah/start", "/_ah/health").permitAll()
+    			.antMatchers("!/_ah", "!/clientmessage")
+                .permitAll()
                 .anyRequest().authenticated()
 				.and()
-//                .userDetailsService(userService)
 				.oauth2Login()
                 .userInfoEndpoint()
                 .oidcUserService(userService);
