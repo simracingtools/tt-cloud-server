082015d478977a8249621df7dd1ef5590247c022
Robert Bausdorf <r.bausdorf@clevershuttle.de>
2020-04-29 20:36:05 +0200

    Token security on client messages

--- src/main/java/de/bausdorf/simcacing/tt/live/clientapi/TeamTacticsClientService.java
+++ src/main/java/de/bausdorf/simcacing/tt/live/clientapi/TeamTacticsClientService.java
@@ -1,9 +1,10 @@
 package de.bausdorf.simcacing.tt.live.clientapi;
 
 import java.util.Map;
+import java.util.Optional;
 
 public interface TeamTacticsClientService {
 
-    String receiveClientData(Map<String, Object> clientMessage);
+    String receiveClientData(Map<String, Object> clientMessage, Optional<String> clientAccessToken);
 
 }
--- src/main/java/de/bausdorf/simcacing/tt/live/impl/TeamTacticsClientServiceImpl.java
+++ src/main/java/de/bausdorf/simcacing/tt/live/impl/TeamTacticsClientServiceImpl.java
@@ -1,14 +1,20 @@
 package de.bausdorf.simcacing.tt.live.impl;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import de.bausdorf.simcacing.tt.live.clientapi.*;
+import de.bausdorf.simcacing.tt.web.security.TtClientRegistrationRepository;
+import de.bausdorf.simcacing.tt.web.security.TtUser;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,35 +27,47 @@ public class TeamTacticsClientServiceImpl implements TeamTacticsClientService {
 
 	static {
 		acceptedVersions.add("1.20");
+		acceptedVersions.add("1.30");
 	}
 
 	private final MessageProcessor processor;
 
-	public TeamTacticsClientServiceImpl(MessageProcessor processor) {
+	private final TtClientRegistrationRepository clientRepository;
+
+	private final Map<String, String> tokenCache;
+
+	public TeamTacticsClientServiceImpl(@Autowired MessageProcessor processor,
+			@Autowired TtClientRegistrationRepository clientRepository) {
+		this.clientRepository = clientRepository;
 		this.processor = processor;
+		this.tokenCache = new HashMap<>();
 	}
 
 	@Override
 	@PostMapping("/clientmessage")
-	public String receiveClientData(@RequestBody Map<String, Object> clientMessage) {
-		ClientMessage msg = this.validateClientMessage(clientMessage);
-		try {
-			processor.processMessage(msg);
-		} catch (IllegalArgumentException e) {
-			log.error("{}: {}", e.getMessage(), msg);
-			StackTraceElement[] trace = e.getStackTrace();
-			log.error("{}({}:{})", trace[0].getMethodName(), trace[0].getClassName(), trace[0].getLineNumber());
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
+	public String receiveClientData(@RequestBody Map<String, Object> clientMessage,
+			@RequestHeader("x-teamtactics-token") Optional<String> clientAccessToken) {
+		if (!clientAccessToken.isPresent()) {
+			log.warn("No x-teamtactics-token header");
+			return "TOKEN_ERROR";
 		}
-		return msg.getType().name();
+		try {
+			ClientMessage msg = this.validateClientMessage(clientMessage, clientAccessToken.get());
+			processMessage(msg);
+			return msg.getType().name();
+		} catch (InvalidClientMessageException e) {
+			log.warn(e.getMessage());
+		}
+		return "VALIDATION_ERROR";
 	}
 
-	private ClientMessage validateClientMessage(Map<String, Object> clientMessage) {
+	private ClientMessage validateClientMessage(Map<String, Object> clientMessage, String accessToken) {
+		String clientId = (String)clientMessage.get(MessageConstants.Message.CLIENT_ID);
+		validateAccessToken(accessToken, clientId);
+
 		String messageType = (String)clientMessage.get(KEY_TYPE);
 		String clientVersion = (String)clientMessage.get(MessageConstants.Message.VERSION);
 		String sessionId = (String)clientMessage.get(MessageConstants.Message.SESSION_ID);
-		String clientId = (String)clientMessage.get(MessageConstants.Message.CLIENT_ID);
 		String teamId = (String)clientMessage.get(MessageConstants.Message.TEAM_ID);
 
 		if( messageType == null ) {
@@ -80,6 +98,44 @@ public class TeamTacticsClientServiceImpl implements TeamTacticsClientService {
 					.build();
 		} catch( Exception e ) {
 			throw new InvalidClientMessageException(e.getMessage());
+		}
+	}
+
+	private void validateAccessToken(String accessToken, String clientId) {
+		String authorizedClientId = tokenCache.get(accessToken);
+		if (authorizedClientId == null) {
+			List<TtUser> users = clientRepository.findByAccessToken(accessToken);
+			if (users.isEmpty()) {
+				throw new InvalidClientMessageException("No token for client id " + clientId);
+			}
+			for (TtUser user : users) {
+				if (user.getClientMessageAccessToken().equalsIgnoreCase(accessToken)
+						&& user.getIRacingId().equalsIgnoreCase(clientId)) {
+					authorizedClientId = clientId;
+					tokenCache.put(accessToken, authorizedClientId);
+					return;
+				}
+			}
+			tokenCache.put(accessToken, "NONE");
+			throw new InvalidClientMessageException("Token could not be validated on client id " + clientId);
+		} else {
+			if (!authorizedClientId.equalsIgnoreCase(clientId)) {
+				throw new InvalidClientMessageException("Token could not be validated on client id " + clientId);
+			}
+		}
+	}
+
+	private void processMessage(ClientMessage msg) {
+		try {
+			processor.processMessage(msg);
+		} catch (InvalidClientMessageException e) {
+			log.warn(e.getMessage());
+		} catch (IllegalArgumentException e) {
+			log.error("{}: {}", e.getMessage(), msg);
+			StackTraceElement[] trace = e.getStackTrace();
+			log.error("{}({}:{})", trace[0].getMethodName(), trace[0].getClassName(), trace[0].getLineNumber());
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
 		}
 	}
 }
