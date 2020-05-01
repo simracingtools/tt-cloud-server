8b1a2a8e21e34dba5d463f17f3bda37004dda691
Robert Bausdorf <r.bausdorf@clevershuttle.de>
2020-05-01 15:11:50 +0200

    Change settings to deploy in app engine flex environment

--- src/main/java/de/bausdorf/simcacing/tt/web/AppEngineController.java
+++ src/main/java/de/bausdorf/simcacing/tt/web/AppEngineController.java
@@ -5,12 +5,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 public class AppEngineController {
+
+	@GetMapping("/_ah/stop")
+	public ResponseEntity<String> appEngineStopRequest() {
+		log.warn("AppEngine send stop request");
+		return new ResponseEntity<>("OK", HttpStatus.OK);
+	}
 
 	@GetMapping("/_ah/start")
 	public ResponseEntity<String> appEngineStartRequest() {
-		return new ResponseEntity("OK", HttpStatus.OK);
+		return new ResponseEntity<>("OK", HttpStatus.OK);
 	}
 
 	@GetMapping("/_ah/health")
