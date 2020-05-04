f4589ebb7828136200e999d5d4b87d4966f78228
Robert Bausdorf <r.bausdorf@clevershuttle.de>
2020-05-04 19:36:07 +0200

    Fix #6: Calculate remaining laps based on last stint average lap time

--- src/main/java/de/bausdorf/simcacing/tt/web/security/WebSecurityConfig.java
+++ src/main/java/de/bausdorf/simcacing/tt/web/security/WebSecurityConfig.java
@@ -6,31 +6,53 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    GoogleUserService userService;
+	private GoogleUserService userService;
+	private ClientRegistrationRepository registrationRepository;
+	private PersistentTokenRepository tokenRepository;
 
-    public WebSecurityConfig(@Autowired GoogleUserService userService) {
-        this.userService = userService;
-    }
+	public WebSecurityConfig(@Autowired GoogleUserService userService,
+			@Autowired	ClientRegistrationRepository registrationRepository,
+			@Autowired PersistentTokenRepository tokenRepository) {
+		super(false);
+		this.userService = userService;
+		this.registrationRepository = registrationRepository;
+		this.tokenRepository = tokenRepository;
+	}
 
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/clientmessage", "/_ah/**", "/live/**", "/app/**", "/liveclient");
-    }
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/clientmessage", "/_ah/**", "/live/**", "/app/**", "/liveclient");
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-    			.antMatchers("!/_ah", "!/clientmessage", "!/live/**", "!/app/**", "!/liveclient")
-                .permitAll()
-                .anyRequest().authenticated()
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("!/_ah", "!/clientmessage", "!/live/**", "!/app/**", "!/liveclient")
+					.permitAll()
+					.anyRequest().authenticated()
+				.and()
+				.rememberMe()
+					.tokenRepository(tokenRepository)
+					.key("irtactics")
+					.tokenValiditySeconds(90000)
 				.and()
 				.oauth2Login()
-                .userInfoEndpoint()
-                .oidcUserService(userService);
-    }
+					.authorizationEndpoint()
+					.authorizationRequestResolver(
+						new CustomAuthorizationRequestResolver(
+								registrationRepository))
+				.and()
+				.userInfoEndpoint()
+				.oidcUserService(userService);
+
+	}
+
 }
