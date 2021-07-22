f700fb07a6e702d0d3ecc9f8faf94ff4d33c1f13
Robert Bausdorf <rbausdorf@gmail.com>
2021-07-22 14:25:03 +0200

    confirmUserRemove re-added

--- src/main/resources/static/assets/js/bs-init.js
+++ src/main/resources/static/assets/js/bs-init.js
@@ -45,6 +45,10 @@ function confirmMemberRemove(index) {
 	$("#member-remove-confirm-" + index).modal('show');
 }
 
+function confirmUserRemove(index) {
+	$("#user-remove-confirm-" + index).modal('show');
+}
+
 function confirmTeamDelete(teamId) {
 	$("#team-delete-confirm-" + teamId).modal('show');
 }
